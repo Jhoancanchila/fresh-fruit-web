e6283b254bddca58efa80be7d03d40d8b8cd5477
jhoancanchila <canchila.jhoan@gmail.com>
2024-02-19 16:45:26 -0500

    [ADD]car icon

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -10,34 +10,54 @@ const Card = ({
   name,
   price,
   idFruit,
-  loading
+  loading,
+  fruitsData
 }) => {
+
+  const handleCar = (id) => {
+    const newDataFruits = fruitsData.map(fruit=>{
+      if(fruit.id === id){
+        return{
+          ...fruit,
+          car: fruit.car ? 0 : 1
+        }
+      }else{
+        return fruit
+      }
+    })
+  };
+
   return (
     <li>
-      <Link to={`/fruits/${idFruit}/${name}`} className="group block overflow-hidden">
+      <div className="group block overflow-hidden">
         {
           loading ? 
           <Skeleton height={350} width='100%'/>
           :
-          <img
-            src={picture}
-            alt={name}
-            className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-            loading="lazy"
-          />
+          <Link to={`/fruits/${idFruit}/${name}`}>
+            <img
+              src={picture}
+              alt={name}
+              className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+              loading="lazy"
+            />
+          </Link>
         }
-        <div className="relative bg-white pt-3">
-          <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
-            {loading ? <Skeleton/> : name}
-          </h3>
+        <div className="flex justify-between items-center mt-4">
+          <div className="relative  pt-3">
+            <h3 className="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4">
+              {loading ? <Skeleton/> : name}
+            </h3>
 
-          <p className="mt-2">
-            <span className="sr-only"> Regular Price </span>
+            <p className="mt-2">
+              <span className="sr-only"> Regular Price </span>
 
-            <span className="tracking-wider text-gray-900"> {loading ? <Skeleton/> : price} </span>
-          </p>
+              <span className="tracking-wider text-gray-900"> {loading ? <Skeleton/> : price} </span>
+            </p>
+          </div>
+          <button onClick={()=>handleCar(idFruit)} className="py-3 px-2 bg-[#EF8043] outline-none rounded">Add to cart</button>
         </div>
-      </Link>
+      </div>
     </li>
   )
 }
