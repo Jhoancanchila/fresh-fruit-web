098773ead6b089ea7796b3d18e8c55a58fdb4cae
jhoancanchila <canchila.jhoan@gmail.com>
2024-02-20 12:16:21 -0500

    [FIXED]close panel detail car

--- src/components/DetailCar/DetailCar.jsx
+++ src/components/DetailCar/DetailCar.jsx
@@ -66,19 +66,19 @@ const DetailCar = ({ functionControlPanel }) => {
         </dl>
         <div className="space-y-4 text-center">
 
-          <a
-            href="#"
+          <button
+            onClick={handleClseModal}
             className="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600"
           >
             Checkout
-          </a>
+          </button>
 
-          <a
-            href="#"
+          <button
+            onClick={handleClseModal}
             className="inline-block text-sm text-gray-500 underline underline-offset-4 transition hover:text-gray-600"
           >
             Continue shopping
-          </a>
+          </button>
         </div>
       </div>
     </div>
